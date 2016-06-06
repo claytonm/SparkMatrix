88a067b7a878e5f682682dd42141f90dd9050878
Clay <claytonm@gmail.com>
2016-06-06 10:51:32 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Matrix"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++=  Seq(
+  "org.apache.spark" %% "spark-core" % "1.6.1"
+)
---
+++ src/main/scala/Matrix/Matrix.scala
@@ -0,0 +1,91 @@
+package SparkMatrix
+
+import org.apache.spark.rdd.RDD
+
+class Matrix(rdd: RDD[(Int, Int, Double)]) {
+
+  def getRDD = rdd
+
+  def transpose(): Matrix = {
+    new Matrix(rdd.
+      map{case (rowIndex, colIndex, value) => (colIndex, rowIndex, value)})
+  }
+
+  def *(that: Matrix): Matrix = {
+
+    val rddL = rdd.
+      map{case (rowIndex, colIndex, value) => (colIndex, (rowIndex, value))}
+    val rddR = that.getRDD.
+      map{case (rowIndex, colIndex, value) => (rowIndex, (colIndex, value))}
+
+    val productRDD = rddL.join(rddR).
+      map{case (colIndexLeft, ((rowIndexLeft, valLeft), (colIndexRight, valueRight)))
+      => ((rowIndexLeft, colIndexRight), valLeft*valueRight)}.
+      reduceByKey(_ + _).
+      map{case ((rowIndex, colIndex), value) => (rowIndex, colIndex, value)}
+
+    new Matrix(productRDD)
+  }
+
+  def rowSum(): RDD[(Int, Double)] = {
+    rdd.
+      map{case (rowIndex, colIndex, value) => (rowIndex, value)}.
+      reduceByKey(_ + _)
+  }
+
+  def colSum(): RDD[(Int, Double)] = {
+    rdd.
+      map{case (rowIndex, colIndex, value) => (colIndex, value)}.
+      reduceByKey(_ + _)
+  }
+
+  def functionApply(function: Double => Double): Matrix = {
+
+    val newRDD = rdd.
+      map{case (rowIndex, colIndex, value) => (rowIndex, colIndex, function(value))}
+
+    new Matrix(newRDD)
+  }
+
+  def getRow(row: Int): Matrix = {
+    new Matrix(
+      rdd.
+        filter{case (rowIndex, colIndex, value) => rowIndex == row}
+    )
+  }
+}
+
+object Matrix {
+
+  def rddToMatrix(rdd: RDD[Array[Double]]): Matrix = {
+
+    val matrixRDD = rdd.
+      zipWithIndex.
+      map{case (vector, rowIndex) => (rowIndex, vector)}.
+      flatMapValues(l => l.zipWithIndex).
+      map{case (rowIndex, (value, colIndex)) => (rowIndex.toInt, colIndex.toInt, value)}
+
+    new Matrix(matrixRDD)
+  }
+
+  def rddToSparseMatrix(rdd: RDD[Array[Double]]): Matrix = {
+
+    val matrixRDD = rdd.
+      zipWithIndex.
+      map{case (vector, rowIndex) => (rowIndex, vector)}.
+      flatMapValues(l => l.zipWithIndex).
+      filter{case (rowIndex, (value, colIndex)) => value > 0}.
+      map{case (rowIndex, (vector, colIndex)) => (rowIndex.toInt, colIndex.toInt, vector)}
+
+    new Matrix(matrixRDD)
+  }
+
+  def diag(rdd: RDD[(Int, Double)]): Matrix = {
+
+    val diagMatrix = rdd.
+      map{case (index, value) => (index, index, value)}
+
+    new Matrix(diagMatrix)
+  }
+}
+
